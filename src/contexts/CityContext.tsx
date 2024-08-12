dc93990af57ae27a801c2afad490a288e15ef96d
joaogkvalho <joaogkvalho@gmail.com>
2024-08-12 14:21:40 -0300

    hooks and routes tested

--- src/contexts/CityContext.tsx
+++ src/contexts/CityContext.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, createContext, useEffect, useState, } from 'react';
 
+import { getStorageCity, saveStorageCity } from '@libs/asyncStorage/cityStorage';
 import { CityProps } from '@services/getCityByNameService';
-import { getStorageCity, removeStorageCity, saveStorageCity } from '@libs/asyncStorage/cityStorage';
 
 type CityContextProviderProps = {
   children: ReactNode
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import { theme } from '@styles/theme';
 import { useCity } from '@hooks/useCity';
 import { Dashboard } from '@screens/Dashboard';
+import { theme } from '@styles/theme';
 
 import { AppRoutes } from './app.routes';
 
